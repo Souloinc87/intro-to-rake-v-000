eb7dc9b1c58f364d4e25ccb4f4636a876e24f2cb
Marcus  = Soulo <souloinc87@gmail.com>
2019-01-04 03:55:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,12 +50,4 @@ class Student
     sql = "SELECT * FROM students"
     DB[:conn].execute(sql)
   end
-
-  namespace :db do
-    desc 'migrate changes to your database'
-    task :migrate => :environment do
-      Student.create_table
-    end
-  end
-
 end
